9b53890908ae1e93be9dfb40b74c978586a24fc3
Guilherme Camargo <cyberiaTCC@gmail.com>
2024-05-24 13:09:35 -0300

    adicionando imagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,16 @@ function updateProfileInfo(profileData) {
 
 }
 
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById = document.getElementById('profile.skills.softSkills')
+ softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>$(skill)</li>`).join('')
+}
+
 
 
 (async () => {
 
   const profileData = await fetchProfileData()
   updateProfileInfo(profileData)
+  updateSoftSkills(profileData)
 })()
