e3cc8987d19c0a95e7fde9a528628947573197ea
Guilherme Camargo <cyberiaTCC@gmail.com>
2024-05-24 10:00:30 -0300

    dados consumidos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 
 
-function fetchProfileData() {
-  const url = ''
-  fetch()
+async function fetchProfileData() {
+  const url = 'https://raw.githubusercontent.com/GuilhermeCamargo1905/Portfolio/main/data/profile.json';
+  const fetching = await fetch(url)
+  return await fetching.json()
 }
 
