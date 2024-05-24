644f8bd3d3143ee0435cd0ac72d0646d80ea599c
Guilherme Camargo <cyberiaTCC@gmail.com>
2024-05-24 15:43:19 -0300

    adicionando idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,10 @@ function updateHardSkills(profileData) {
   hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateLanguages(profileData) {
+  const languages = document.getElementById(profile.languages)
+  languages.innerHTML = profileData.languages.map(language => `<li>$(language)</li>`).join('')
+}
 
 
 
@@ -43,4 +47,5 @@ function updateHardSkills(profileData) {
   updateProfileInfo(profileData)
   updateSoftSkills(profileData)
   updateHardSkills(profileData)
+  updateLanguages(profileData)
 })()
